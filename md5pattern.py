d89741156da9d5c73fc840f4fab69182608d1f3d
Urmila Kalita <49206968+urmi11a@users.noreply.github.com>
2020-10-03 21:35:25 +0530

    Update md5pattern.py

--- md5pattern.py
+++ md5pattern.py
@@ -1,11 +1,11 @@
 import hashlib
 
-target = '8e7c9e'
+target = '8e7c9e'  
 candidate = 0
 while True:
     plaintext = str(candidate)
-    hash = hashlib.md5(plaintext.encode('ascii')).hexdigest()
+    hash = hashlib.md5(plaintext.encode('ascii')).hexdigest() #ascii encoding then md5 encoding
     if hash[:6] == target:
-        print('plaintext:"' + plaintext + '", md5:' + hash)
+        print('plaintext:"' + plaintext + '", md5:' + hash)  # md5 pattern found
         break
     candidate = candidate + 1
